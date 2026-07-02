743abd33ec0df9e781d65fbb70ae0982157b13f1
Pattaratida Phongthep <Pattaratida.06032551@gmail.com>
2026-07-02 16:00:46 +0700

    Add VSCode configuration files and initial C program
    - Created c_cpp_properties.json for C/C++ IntelliSense configuration.
    - Added launch.json for debugging configurations.
    - Introduced settings.json for terminal and file associations.
    - Implemented tasks.json for build tasks with MSYS2 and Raylib.
    - Added 03-Setup-VSCode.ps1 for setup automation.
    - Created InstallationLog.txt to log installation details.
    - Added hello.c as a sample C program with basic output.

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("สวัสดีค่ะ! หนู ภัทรธิดา พงษ์เทพ\n");
+    printf("รหัสนักศึกษา: 692110160\n");
+    printf("วิชา: DG11 Computer Programming\n");
+    return 0;
+}
